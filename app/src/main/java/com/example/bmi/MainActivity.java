ba68b6ec71c6a2b9fa413c727443d5a471adb114
malu <windy005@126.com>
2015-03-15 17:44:50 +0800

    Add the getSharedPreference

--- app/src/main/java/com/example/bmi/MainActivity.java
+++ app/src/main/java/com/example/bmi/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bmi;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -27,6 +28,7 @@ public class MainActivity extends ActionBarActivity {
     Button submitButton;
     private EditText mOutEditText;
     private ImageView mImage;
+    private Spinner spinnerFeet,spinnerInches;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,11 +48,11 @@ public class MainActivity extends ActionBarActivity {
         feetArray = getResources().getStringArray(R.array.feet);
         inchesArray = getResources().getStringArray(R.array.inches);
 
-        Spinner spinnerFeet = (Spinner) findViewById(R.id.spinner_feet);
+        spinnerFeet = (Spinner) findViewById(R.id.spinner_feet);
         ArrayAdapter<String> adapterFeet = new ArrayAdapter<String>(this,
                 R.layout.dropdown_item, feetArray);
         spinnerFeet.setAdapter(adapterFeet);
-        Spinner spinnerInches = (Spinner) findViewById(R.id.spinner_inches);
+        spinnerInches = (Spinner) findViewById(R.id.spinner_inches);
         ArrayAdapter<String> adapterInches = new ArrayAdapter<String>(this,
                 R.layout.dropdown_item, inchesArray);
         spinnerInches.setAdapter(adapterInches);
@@ -81,6 +83,7 @@ public class MainActivity extends ActionBarActivity {
 }
     public void calcBMI(View view) {
         String weight = vWeight.getText().toString();
+        savePreferences(feet-1, inches, weight);//why minus 1? feet stand your height, feet-1 means its corresponding spinner position.
         Intent intent = new Intent(this, ReportActivity.class);
         Bundle bundle = new Bundle();
         bundle.putInt("feet", feet);
@@ -91,7 +94,19 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
+    public void savePreferences(int f, int i, String w) {
+        SharedPreferences pref = getSharedPreferences("BMI", MODE_PRIVATE);
+        pref.edit().putInt("feet", f).commit();
+        pref.edit().putInt("inches", i).commit();
+        pref.edit().putString("weight", w).commit();
+    }
 
+    public void loadPreferences() {
+        SharedPreferences pref = getSharedPreferences("BMI", MODE_PRIVATE);
+        spinnerFeet.setSelection(pref.getInt("feet", 0));
+        spinnerInches.setSelection(pref.getInt("inches", 0));
+        vWeight.setText(pref.getString("weight", "0"));
+    }
 
 //    public void calcBMI(View view) {
 //        String height = vHeight.getText().toString();
@@ -183,6 +198,13 @@ public class MainActivity extends ActionBarActivity {
                 return super.onContextItemSelected(item);
         }
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loadPreferences();
+    }
 }
+
+
 
 
